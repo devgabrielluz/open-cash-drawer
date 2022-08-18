d20f299464a3f161c66dc8853d6110b68dd7a6ce
Gabriel Luz <87550203+devgabrielluz@users.noreply.github.com>
2022-08-18 20:51:42 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.IO;
+using System.Runtime.InteropServices;
+
+class RawPrinterHelper
+{
+  // Structure and API declarions:
+  [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+  public class DOCINFOA
+  {
+    [MarshalAs(UnmanagedType.LPStr)]
+    public string pDocName;
+    [MarshalAs(UnmanagedType.LPStr)]
+    public string pOutputFile;
+    [MarshalAs(UnmanagedType.LPStr)]
+    public string pDataType;
+  }
+  [DllImport("winspool.Drv", EntryPoint = "OpenPrinterA", SetLastError = true, CharSet = CharSet.Ansi, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
+  public static extern bool OpenPrinter([MarshalAs(UnmanagedType.LPStr)] string szPrinter, out IntPtr hPrinter, IntPtr pd);
+
+  [DllImport("winspool.Drv", EntryPoint = "ClosePrinter", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
+  public static extern bool ClosePrinter(IntPtr hPrinter);
+
+  [DllImport("winspool.Drv", EntryPoint = "StartDocPrinterA", SetLastError = true, CharSet = CharSet.Ansi, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
+  public static extern bool StartDocPrinter(IntPtr hPrinter, Int32 level, [In, MarshalAs(UnmanagedType.LPStruct)] DOCINFOA di);
+
+  [DllImport("winspool.Drv", EntryPoint = "EndDocPrinter", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
+  public static extern bool EndDocPrinter(IntPtr hPrinter);
+
+  [DllImport("winspool.Drv", EntryPoint = "StartPagePrinter", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
+  public static extern bool StartPagePrinter(IntPtr hPrinter);
+
+  [DllImport("winspool.Drv", EntryPoint = "EndPagePrinter", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
+  public static extern bool EndPagePrinter(IntPtr hPrinter);
+
+  [DllImport("winspool.Drv", EntryPoint = "WritePrinter", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
+  public static extern bool WritePrinter(IntPtr hPrinter, IntPtr pBytes, Int32 dwCount, out Int32 dwWritten);
+
+  // SendBytesToPrinter()
+  // When the function is given a printer name and an unmanaged array
+  // of bytes, the function sends those bytes to the print queue.
+  // Returns true on success, false on failure.
+  public static bool SendBytesToPrinter(string szPrinterName, IntPtr pBytes, Int32 dwCount)
+  {
+    Int32 dwError = 0, dwWritten = 0;
+    IntPtr hPrinter = new IntPtr(0);
+    DOCINFOA di = new DOCINFOA();
+    bool bSuccess = false; // Assume failure unless you specifically succeed.
+
+    di.pDocName = "Microsoft Print to PDF";
+    di.pDataType = "RAW";
+
+    // Open the printer.
+    if (OpenPrinter(szPrinterName.Normalize(), out hPrinter, IntPtr.Zero))
+    {
+      // Start a document.
+      if (StartDocPrinter(hPrinter, 1, di))
+      {
+        // Start a page.
+        if (StartPagePrinter(hPrinter))
+        {
+          // Write your bytes.
+          bSuccess = WritePrinter(hPrinter, pBytes, dwCount, out dwWritten);
+          EndPagePrinter(hPrinter);
+        }
+        EndDocPrinter(hPrinter);
+      }
+      ClosePrinter(hPrinter);
+    }
+    // If you did not succeed, GetLastError may give more information
+    // about why not.
+    if (bSuccess == false)
+    {
+      dwError = Marshal.GetLastWin32Error();
+    }
+    return bSuccess;
+  }
+
+  private static void Main()
+  {
+    byte[] codeOpenCashDrawer = new byte[] { 27, 112, 48, 55, 121 };
+    IntPtr pUnmanagedBytes = new IntPtr(0);
+    pUnmanagedBytes = Marshal.AllocCoTaskMem(5);
+    Marshal.Copy(codeOpenCashDrawer, 0, pUnmanagedBytes, 5);
+    RawPrinterHelper.SendBytesToPrinter("<Your Printer Name>", pUnmanagedBytes, 5);
+    Marshal.FreeCoTaskMem(pUnmanagedBytes);
+  }
+
+}
